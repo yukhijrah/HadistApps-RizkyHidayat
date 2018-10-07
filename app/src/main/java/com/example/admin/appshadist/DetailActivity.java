51e65ed8a7951e58da1ec7ed2a88f0ffe0b454e3
rizkypjt <rizkyhidayatpanjaitan97@gmail.com>
2018-10-07 21:17:22 +0700

    Apps Hadist praktikum 3

---
+++ app/src/main/java/com/example/admin/appshadist/DetailActivity.java
@@ -0,0 +1,18 @@
+package com.example.admin.appshadist;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by admin on 07/10/2018.
+ */
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
