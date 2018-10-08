57dd3b5960a0aa707cad4468440269fa4f8df742
rizkypjt <rizkyhidayatpanjaitan97@gmail.com>
2018-10-08 14:25:32 +0700

    plast dan Ricyclerview

--- app/src/main/java/com/example/admin/appshadist/SplashActivity.java
+++ app/src/main/java/com/example/admin/appshadist/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.example.admin.appshadist;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
@@ -10,9 +12,31 @@ import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity{
 
+    private static final int TIGA_DETIK = 3000; //Satuannya milisecond
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        countDown();
     }
+
+    private void countDown(){
+        Handler handler = new Handler();
+        Runnable menunggu = new Runnable() {
+            @Override
+            public void run() {
+                gotoMain();
+            }
+        };
+
+        handler.postDelayed(menunggu, TIGA_DETIK);
+    }
+
+    private void gotoMain(){
+        Intent intentMain = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(intentMain);
+        finish();
+    }
+
 }
